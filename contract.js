c97a4892146a4208232b074efeb8701466b621de
Christian Ulrich <christian@reindex.ch>
2022-11-19 17:44:32 +0100

    Added Balance Check Functionality

--- contract.js
+++ contract.js
@@ -1,26 +1,49 @@
 window.addEventListener("load", (event) => {
 
 // Hide Option Buttons
-//document.getElementById('accounts').style.visibility= 'hidden';
+document.getElementById('Account-section').style.visibility= 'visible';
+
+
 
 //Get Buttons Button
 const connectButton = document.getElementById("connect");
 const getAccountsButton = document.getElementById("accounts");
-const getAccountsResult = 0;
+
+//Check if User is connected
+
 
 
 //Button Click Connect to Metamask
 connectButton.addEventListener('click', async() => {
-  await ethereum.request({ method: 'eth_requestAccounts' });
+    try {
+        await ethereum.request({ method: 'eth_requestAccounts' });
+    } catch (error) {
+        if (error.code === 4001) {
+            console.log('User Rejected connection to Metamask ERRORCODE 4001');
+        } else {
+            console.error(error);
+        }
+    }
 });
 
 
 
 getAccountsButton.addEventListener('click', async () => {
-    //we use eth_accounts because it returns a list of addresses owned by us.
+    //Call ETH Accounts
     const accounts = await ethereum.request({ method: 'eth_accounts' });
-    //We take the first address in the array of addresses and display it
-    getAccountsResult.innerHTML = accounts[0] || 'Not able to get accounts';
+    const balance = await ethereum.request({ method: 'eth_getBalance', params:[accounts[0],"latest"]  });
+    
+    //Display first Adress connected
+    const AccountAdress = accounts[0] || 'Not able to get accounts // Please Connect your Wallet';
+    const AccountBalance = balance[0] || 'Not able to get accounts // Please Connect your Wallet';
+
+     //Write to Box
+     document.getElementById('adress').innerText = AccountAdress
+     document.getElementById('balance').innerText = AccountBalance
+
+   
+
+     
   });
 
 
@@ -32,14 +55,6 @@ getAccountsButton.addEventListener('click', async () => {
 
 
 
-
-
-
-
-
-
-
-
-
-
 });
+
+
