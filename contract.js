53d72bcb0e95ac82d8d157c2d689a40afb910578
Christian Ulrich <christian@reindex.ch>
2022-11-19 16:45:25 +0100

    Account Func

--- contract.js
+++ contract.js
@@ -6,6 +6,7 @@ window.addEventListener("load", (event) => {
 //Get Buttons Button
 const connectButton = document.getElementById("connect");
 const getAccountsButton = document.getElementById("accounts");
+const getAccountsResult = 0;
 
 
 //Button Click Connect to Metamask
@@ -14,6 +15,13 @@ connectButton.addEventListener('click', async() => {
 });
 
 
+
+getAccountsButton.addEventListener('click', async () => {
+    //we use eth_accounts because it returns a list of addresses owned by us.
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    //We take the first address in the array of addresses and display it
+    getAccountsResult.innerHTML = accounts[0] || 'Not able to get accounts';
+  });
 
 
 
