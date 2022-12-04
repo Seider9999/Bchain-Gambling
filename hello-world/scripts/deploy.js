9099787193458e807933cb536ad44767479e8f80
Christian Ulrich <christian@reindex.ch>
2022-12-04 21:18:30 +0100

    Up to date and deployment successfull

---
+++ hello-world/scripts/deploy.js
@@ -0,0 +1,9 @@
+async function main() {
+    const HelloWorld = await ethers.getContractFactory("CoinFlip")
+  
+    // Start deployment, returning a promise that resolves to a contract object
+    const hello_world = await HelloWorld.deploy();
+    console.log("Contract deployed to address:", hello_world.address)
+  }
+  
+  main()
